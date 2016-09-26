421420526c213800f94551c41f6d83396c32ce1f
Roman Doroshenko <r.m.doroshenko@gmail.com>
2016-09-26 22:16:28 +0300

    Variable rename r -> rental

--- store.js
+++ store.js
@@ -4,16 +4,16 @@ function statement(customer, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
-  for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
+  for (let rental of customer.rentals) {
+    let movie = movies[rental.movieID];
     let thisAmount = 0;
 
     // determine amount for each movie
     switch (movie.code) {
       case "regular":
         thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
+        if (rental.days > 2) {
+          thisAmount += (rental.days - 2) * 1.5;
         }
         break;
       case "new":
@@ -21,7 +21,7 @@ function statement(customer, movies) {
         break;
       case "childrens":
         thisAmount = 1.5;
-        if (r.days > 3) {
+        if (rental.days > 3) {
           thisAmount += (r.days - 3) * 1.5;
         }
         break;
